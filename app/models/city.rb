8fb53173c38884776f2f38d4941656bdf8aae8d4
stephen & steven <student@epicodus.com>
2015-07-22 14:42:07 -0700

    add spec and code to validate the presence of state for City, all specs passing.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,4 @@
 class City < ActiveRecord::Base
   validates :name, :presence => true
+  validates :state, :presence => true
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe City do
   it {should validate_presence_of :name}
+  it {should validate_presence_of :state }
 end
